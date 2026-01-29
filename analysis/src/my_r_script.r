ac6837a31a60ca3e8ae419631a7abd59ecbfa434
hirsch25 <hirsch25@uab.edu>
2026-01-29 15:29:31 -0600

    Reorg to good enough structure

---
+++ analysis/src/my_r_script.r
@@ -0,0 +1,63 @@
+#this is a comment
+101 + 101
+3 + 5 * 10
+
+# help me remember PEMDAS
+3 + (5*10)
+
+# I want to add first
+(3 + 5) * 10
+
+#do I need multiplication symmbol with paratheses? yes
+(3 + 5) 10
+
+#exponents 
+3 ^ 2
+
+#are these equal? True
+10 == 10
+
+#are these equal? False
+9 == 10
+
+#computers aren't great with decimals
+10.01 == 10.01
+10.02 == 10.01
+10.01 == 10.0000000001
+10.0 == 10.00000000000000000001
+
+#what do fractions look like here
+2/10000
+500000000/2
+
+#R has functions
+#get working directory
+getwd()
+#math functions
+sin(1)
+
+#R is like math
+#this is the assignment manager: what ever computation happens on the right side, I want you to store it as x
+x <- 1/40
+x = 1/100
+#short cut for assignment operator
+# ? can be used to look up things in the help tab via the console
+
+#R is a vectorized language
+#this says I want a vector with 1 followed by 2, 3, 4, 5
+1:5
+x <- 3:10
+
+#vectors can be used in computations
+2 ^ (x)
+
+#ls is similar to ls in the terminal to know what the vectors are in the global environment
+
+#rm can also be used like the terminal
+
+#install packages
+install.packages("tidyverse")
+library(tidyverse)
+install.packages("dplyr")
+library(dplyr)
+
