91fffcb30f04aa5f8b1d3bd9aa859cad6f28936f
hirsch25 <hirsch25@uab.edu>
2026-02-02 14:16:38 -0600

    adding reports_knitr.r

---
+++ analysis/src/reports_knitr.r
@@ -0,0 +1,8 @@
+#reports are a less cumbersome way to share analysis summaries and include with scripts and figures
+#this summarizes the analysis and put them into a report
+#R markdown is used to create these reports, can support R code and can be converted to html
+#package within R markdown that is used is knitr
+#if you get an errors mean you just need to re-run a section not the whole thing
+
+#create new file, create R markdown, keep it at HTML
+
